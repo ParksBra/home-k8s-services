06efad6e5cd59ad8433d6fe43929f5c91aa7c7b1
Brandon <brandon@parksbra.com>
2025-11-26 15:01:13 -0800

    move away from ephemeral secrets

--- terraform/homeassistant_environment.tf
+++ terraform/homeassistant_environment.tf
@@ -8,12 +8,11 @@ module "homeassistant_environment" {
   ingress_class_name = local.ingress_class_name
 
   mosquitto_admin_username = local.mosquitto_admin_username
-  mosquitto_admin_password = ephemeral.infisical_secret.mosquitto_admin_password.value
+  mosquitto_admin_password = data.infisical_secrets.environment.secrets[local.mosquitto_admin_password_secret_name].value
 
-  acme_email = ephemeral.infisical_secret.acme_email.value
-  cloudflare_email = ephemeral.infisical_secret.cloudflare_email.value
-  cloudflare_api_token = ephemeral.infisical_secret.cloudflare_api_token.value
-
+  acme_email = data.infisical_secrets.environment.secrets[local.acme_email_secret_name].value
+  cloudflare_email = data.infisical_secrets.environment.secrets[local.cloudflare_email_secret_name].value
+  cloudflare_api_token = data.infisical_secrets.environment.secrets[local.cloudflare_api_token_secret_name].value
   parent_domain = local.parent_domain
 
   zfs_pool_name = local.zfs_pool_name
--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,26 +1,4 @@
-ephemeral "infisical_secret" "mosquitto_admin_password" {
-  name         = local.mosquitto_admin_password_secret_name
-  env_slug     = var.infisical_environment
-  workspace_id  = var.infisical_project_id
-  folder_path  = "/"
-}
-
-ephemeral "infisical_secret" "cloudflare_email" {
-  name         = local.cloudflare_email_secret_name
-  env_slug     = var.infisical_environment
-  workspace_id  = var.infisical_project_id
-  folder_path  = "/"
-}
-
-ephemeral "infisical_secret" "cloudflare_api_token" {
-  name         = local.cloudflare_api_token_secret_name
-  env_slug     = var.infisical_environment
-  workspace_id  = var.infisical_project_id
-  folder_path  = "/"
-}
-
-ephemeral "infisical_secret" "acme_email" {
-  name         = local.acme_email_secret_name
+data "infisical_secrets" "environment" {
   env_slug     = var.infisical_environment
   workspace_id  = var.infisical_project_id
   folder_path  = "/"
